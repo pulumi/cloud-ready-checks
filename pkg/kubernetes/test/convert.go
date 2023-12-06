09f4f60909720a953113ef656ed9c03f6bb321c5
Ramon Quitales <ramon@pulumi.com>
2023-12-05 16:27:43 -0800

    Upgrade k8s libraries to v0.29.0-rc.1

--- pkg/kubernetes/test/convert.go
+++ pkg/kubernetes/test/convert.go
@@ -42,8 +42,8 @@ import (
 	eventsv1 "k8s.io/api/events/v1"
 	eventsv1beta1 "k8s.io/api/events/v1beta1"
 	extensionsv1beta1 "k8s.io/api/extensions/v1beta1"
-	flowcontrolv1alpha1 "k8s.io/api/flowcontrol/v1alpha1"
 	flowcontrolv1beta1 "k8s.io/api/flowcontrol/v1beta1"
+	flowcontrolv1beta3 "k8s.io/api/flowcontrol/v1beta3"
 	imagepolicyv1alpha1 "k8s.io/api/imagepolicy/v1alpha1"
 	networkingv1 "k8s.io/api/networking/v1"
 	networkingv1beta1 "k8s.io/api/networking/v1beta1"
@@ -92,8 +92,8 @@ var groups = []runtime.SchemeBuilder{
 	eventsv1.SchemeBuilder,
 	eventsv1beta1.SchemeBuilder,
 	extensionsv1beta1.SchemeBuilder,
-	flowcontrolv1alpha1.SchemeBuilder,
 	flowcontrolv1beta1.SchemeBuilder,
+	flowcontrolv1beta3.SchemeBuilder,
 	imagepolicyv1alpha1.SchemeBuilder,
 	networkingv1.SchemeBuilder,
 	networkingv1beta1.SchemeBuilder,
